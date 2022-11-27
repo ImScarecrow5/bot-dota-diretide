31c6aa906f7991f23370efa59fbb9e523adb1f8b
Антон <vriilux@yandex.ru>
2022-11-27 23:54:01 +0300

    добавил класс

--- main.py
+++ main.py
@@ -5,83 +5,91 @@ import time
 import cv2
 import pyautogui
 
-root = tkinter.Tk()
-width1 = root.winfo_screenwidth()
-height1 = root.winfo_screenheight()
-print(width1, height1)
+class Diretide:
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.width1 = self.root.winfo_screenwidth()
+        self.height1 = self.root.winfo_screenheight()
+        print(self.width1, self.height1)
 
-template = cv2.imread('screenshot/go.png', 0)  # читаем картинку
-w, h = template.shape[::-1]  # высота и ширина скриншота
+        self.template = cv2.imread('screenshot/go.png', 0)  # читаем картинку
+        self.w, self.h = self.template.shape[::-1]  # высота и ширина скриншота
 
-photo2 = cv2.imread('screenshot/agree.png', 0)  # читаем картинку
-w2, h2 = photo2.shape[::-1]  # высота и ширина скриншота
+        self.photo2 = cv2.imread('screenshot/agree.png', 0)  # читаем картинку
+        self.w2, self.h2 = self.photo2.shape[::-1]  # высота и ширина скриншота
 
-print('1')
-time.sleep(1)
-print('2')
-time.sleep(1)
-print('3')
-time.sleep(1)
+        print('1')
+        time.sleep(1)
+        print('2')
+        time.sleep(1)
+        print('3')
+        time.sleep(1)
+
+    def found_agree(self):
+        self.flag2 = True
+        while self.flag2:  # Пытаемся найти поиск
+            print('not found agree game')
+            self.screen_base = ImageGrab.grab(bbox=(0, 0, self.width1, self.height1))  # resolution
+            self.screen_base.save('screenshot/base_screen.png')
+
+            self.img_rgb2 = cv2.imread('screenshot/base_screen.png')  # Конвертируем пнг для opencv
+            self.img_gray2 = cv2.cvtColor(self.img_rgb2, cv2.COLOR_BGR2GRAY)  # чтобы быстрее искалось
+
+            self.res2 = cv2.matchTemplate(self.img_gray2, self.photo2, cv2.TM_CCOEFF_NORMED)  # поиск
+            self.loc2 = np.where(self.res2 >= 0.9)  # координаты
+
+            for pt in zip(*self.loc2[::-1]):
+                self.x = int(pt[0])
+                self.y = int(pt[1])
+                self.agree_screen = ImageGrab.grab(bbox=(self.x, self.y, self.x + self.w2, self.y + self.h2))
+                self.agree_screen.save('screenshot/agree_screen.png')
+                print('found agree game!')  # Приняли игру
+                pyautogui.moveTo(self.x + 10, self.y + 5)
+                self.flag2 = False
+
+    def found_start(self):
+        self.flag = True
+        while self.flag:  # Пытаемся найти игру
+            print('not found start game')
+            self.screen_base = ImageGrab.grab(bbox=(0, 0, self.width1, self.height1))  # resolution
+            self.screen_base.save('D:/pythonptoject/test/screenshot/base_screen.png')
+
+            self.img_rgb = cv2.imread('screenshot/base_screen.png')  # Конвертируем пнг для opencv
+            self.img_gray = cv2.cvtColor(self.img_rgb, cv2.COLOR_BGR2GRAY)  # чтобы быстрее искалось
+
+            self.res = cv2.matchTemplate(self.img_gray, self.template, cv2.TM_CCOEFF_NORMED)  # поиск
+            self.loc = np.where(self.res >= 0.9)  # координаты
+
+            for pt in zip(*self.loc[::-1]):
+                self.x = int(pt[0])
+                self.y = int(pt[1])
+                self.start_screen = ImageGrab.grab(bbox=(self.x, self.y, self.x + self.w, self.y + self.h))
+                self.start_screen.save('screenshot/start_screen.png')
+                print('found start game!')
+                pyautogui.moveTo(self.x + 10, self.y + 5)
+                pyautogui.mouseUp()
+                time.sleep(0.4)
+                pyautogui.mouseDown()
+                pyautogui.mouseUp()
+                time.sleep(0.2)
+                pyautogui.mouseDown()
+                pyautogui.mouseUp()
+                time.sleep(0.2)
+                pyautogui.mouseDown()
+                pyautogui.moveTo(self.x + 210, self.y + 15)
+                time.sleep(2)
+                pyautogui.mouseUp()
+                time.sleep(0.2)
+                pyautogui.mouseDown()
+                time.sleep(0.2)
+                pyautogui.mouseUp()
+                time.sleep(0.2)
+                pyautogui.mouseDown()
+                print('click start game')  # Запустили поиск
+                self.found_agree()
+                self.flag = False
 
 
-flag = True
-while flag:  # Пытаемся найти игру
-    print('not found start game')
-    screen_base = ImageGrab.grab(bbox=(0, 0, width1, height1))  # resolution
-    screen_base.save('D:/pythonptoject/test/screenshot/base_screen.png')
-
-    img_rgb = cv2.imread('screenshot/base_screen.png')  # Конвертируем пнг для opencv
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)  # чтобы быстрее искалось
-
-    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)  # поиск
-    loc = np.where(res >= 0.9)  # координаты
-
-    for pt in zip(*loc[::-1]):
-        x = int(pt[0])
-        y = int(pt[1])
-        start_screen = ImageGrab.grab(bbox=(x, y, x + w, y + h))
-        start_screen.save('screenshot/start_screen.png')
-        print('found start game!')
-        pyautogui.moveTo(x + 10, y + 5)
-        pyautogui.mouseUp()
-        time.sleep(0.4)
-        pyautogui.mouseDown()
-        pyautogui.mouseUp()
-        time.sleep(0.2)
-        pyautogui.mouseDown()
-        pyautogui.mouseUp()
-        time.sleep(0.2)
-        pyautogui.mouseDown()
-        pyautogui.moveTo(x + 210, y + 15)
-        time.sleep(2)
-        pyautogui.mouseUp()
-        time.sleep(0.2)
-        pyautogui.mouseDown()
-        time.sleep(0.2)
-        pyautogui.mouseUp()
-        time.sleep(0.2)
-        pyautogui.mouseDown()
-        print('click start game')  # Запустили поиск
-        flag = False
-
-
-flag2 = True
-while flag2:  # Пытаемся найти поиск
-    print('not found start game')
-    screen_base = ImageGrab.grab(bbox=(0, 0, width1, height1))  # resolution
-    screen_base.save('screenshot/base_screen.png')
-
-    img_rgb2 = cv2.imread('screenshot/base_screen.png')  # Конвертируем пнг для opencv
-    img_gray2 = cv2.cvtColor(img_rgb2, cv2.COLOR_BGR2GRAY)  # чтобы быстрее искалось
-
-    res2 = cv2.matchTemplate(img_gray2, photo2, cv2.TM_CCOEFF_NORMED)  # поиск
-    loc2 = np.where(res2 >= 0.9)  # координаты
-
-    for pt in zip(*loc2[::-1]):
-        x = int(pt[0])
-        y = int(pt[1])
-        agree_screen = ImageGrab.grab(bbox=(x, y, x + w2, y + h2))
-        agree_screen.save('screenshot/agree_screen.png')
-        print('found agree game!')  # Приняли игру
-        pyautogui.moveTo(x + 10, y + 5)
-        flag2 = False
+if __name__ == '__main__':
+    pt = Diretide()
+    pt.found_start()
